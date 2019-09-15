2e9fd34bcdd85ad9e63d92c51acd38db8e67ebcd
Shane Yu <shane@composedcreative.com>
2019-09-15 11:37:25 -0400

    Small Updates

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import store from './store'
 
 // Library Imports
 import './library-imports/axios'
-import './library-imports/clickoutside'
-// import './library-imports/icons'
 
 Vue.config.productionTip = false
 
